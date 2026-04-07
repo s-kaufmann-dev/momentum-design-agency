210437e85c9423cb547f3ab3bacff9167166471b
s-kaufmann-dev <soufian.kaufmann@hotmail.de>
2026-04-07 12:57:28 +0200

    Configure Cloudflare adapter and set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: cloudflare(),
   integrations: [tailwind()],
 });
